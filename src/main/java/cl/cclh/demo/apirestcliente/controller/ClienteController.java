7de7d1ec25d76fa001bf238accfc69c165954ec8
unknown <contacto.luisoliveros@gmail.com>
2022-11-05 20:46:58 -0300

    add

--- src/main/java/cl/cclh/demo/apirestcliente/controller/ClienteController.java
+++ src/main/java/cl/cclh/demo/apirestcliente/controller/ClienteController.java
@@ -11,9 +11,9 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import cl.cclh.demo.apirestcliente.exception.ModeloNotFoundException;
@@ -49,10 +49,22 @@ public class ClienteController {
 		    return new ResponseEntity<>(objeto, HttpStatus.CREATED);	
 	}
 	
-	@PutMapping
+	@RequestMapping(produces = "application/json", method=RequestMethod.PUT)
 	public ResponseEntity<Cliente> modificar(@Valid @RequestBody Cliente cliente) {
-		Cliente objeto = clienteRepo.save(cliente);
-		return new ResponseEntity<Cliente>(objeto, HttpStatus.OK);
+		Optional<Cliente> data = clienteRepo.findByRut(cliente.getRut());
+		if (data.isPresent()) {
+			Cliente objeto = data.get();
+			objeto.setDv(cliente.getDv());
+			objeto.setNombres(cliente.getNombres());
+			objeto.setApellidos(cliente.getApellidos());
+			objeto.setDomicilio(cliente.getDomicilio());
+			objeto.setTelefono(cliente.getTelefono());
+			objeto.setEmail(cliente.getEmail());
+			objeto.setSexo(cliente.getSexo());
+		    return new ResponseEntity<>(clienteRepo.save(objeto), HttpStatus.OK);
+		} else {
+			throw new ModeloNotFoundException("Cliente no encontrado con Rut: " + cliente.getRut());
+		}
 	}
 	
 	@DeleteMapping("/{rut}")
